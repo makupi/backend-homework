75120e0b6aa999079eb698ea02fc1ca948b54eaf
makubob <makupi@icloud.com>
2021-03-05 20:41:02 +0100

    Create SQLite tables if not exists

--- storage/sqliteStorage.go
+++ storage/sqliteStorage.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"database/sql"
+	"errors"
 	"github.com/makupi/backend-homework/models"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
@@ -17,21 +18,60 @@ func NewSqliteStorage() *SqliteStorage {
 		log.Fatal(err)
 	}
 	storage := SqliteStorage{DB: db}
+	err = storage.createTables()
+	if err != nil {
+		log.Fatal(err)
+	}
 	return &storage
 }
 
-func (s *SqliteStorage) List() []models.Question {
+// creates tables questions and options
+// questions:
+// | id: pkey, int | body: text |
+// options:
+// | id: pkey, int | body: text | correct: bool | question_id: fkey(questions.id), int |
+func (s *SqliteStorage) createTables() error {
+	tables := []string{
+		`CREATE TABLE IF NOT EXISTS "questions" (
+		"ID" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"question" TEXT
+		);`,
+		`CREATE TABLE IF NOT EXISTS "options" (
+			"ID" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			"QUESTION_ID" INTEGER NOT NULL,
+			"OPTION" TEXT,
+			"CORRECT" BOOLEAN,
+			CONSTRAINT fk_questions
+				FOREIGN KEY (QUESTION_ID)
+				REFERENCES questions(ID)
+				ON DELETE CASCADE
+		);`,
+	}
 
+	for _, table := range tables {
+		_, err := s.DB.Exec(table)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *SqliteStorage) List() []models.Question {
+	return nil
 }
 
 func (s *SqliteStorage) Add(question models.Question) (models.Question, error) {
-
+	var q models.Question
+	return q, errors.New("not implemented")
 }
 
 func (s *SqliteStorage) Get(id int) (models.Question, error) {
-
+	var q models.Question
+	return q, errors.New("not implemented")
 }
 
 func (s *SqliteStorage) Update(id int, question models.Question) (models.Question, error) {
-
+	var q models.Question
+	return q, errors.New("not implemented")
 }
